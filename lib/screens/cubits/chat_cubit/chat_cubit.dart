c8f223bec4a664a49083b82f65e7aa777a34fcb9
Your Name <your.email@example.com>
2025-05-20 16:54:46 +0300

    adding logic to cubit state

--- lib/screens/cubits/chat_cubit/chat_cubit.dart
+++ lib/screens/cubits/chat_cubit/chat_cubit.dart
@@ -1,8 +1,28 @@
 import 'package:bloc/bloc.dart';
+import 'package:chat_app/constant.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:meta/meta.dart';
 
 part 'chat_state.dart';
 
 class ChatCubit extends Cubit<ChatState> {
   ChatCubit() : super(ChatInitial());
+  CollectionReference messages =
+      FirebaseFirestore.instance.collection(KMessagesCollections);
+
+  void sendMessage({required String message, required String email}) {
+    messages.add(
+      {
+        KMessage: message,
+        KCreatedAt: DateTime.now(),
+        'id': email,
+      },
+    );
+  }
+
+  void getMessage(){
+    messages.orderBy(KCreatedAt, descending: true).snapshots().listen((event) {
+      emit(ChatSuccess());
+    });
+  }
 }
